c85a537a97665a9a54dc3f138fa24d842fd8f59a
Maor-Gross <maorgross247@gmail.com>
2025-08-10 13:17:24 +0300

    update cors

--- middlewares/cors.js
+++ middlewares/cors.js
@@ -5,7 +5,7 @@ const corsmiddleware = cors({
     "http://127.0.0.1:5173",
     "http://localhost:5173",
     "https://www.cardsproject.co.il",
-    "https://full-stack-z4c4.onrender.com"
+    "https://vonclick.onrender.com/"
   ], methods: ['GET', 'POST', 'PUT', 'OPTIONS', 'PATCH'],
   allowedHeaders: ['Content-Type', 'Authorization', `x-auth-token`],
   credentials: true,
